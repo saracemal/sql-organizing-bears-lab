b6616a56c3542391ab62bc34e1a1aa25f2fc1e39
Sara Cemal <you@example.com>
2020-11-03 21:43:51 -0500

    wip

---
+++ lib/create.sql
@@ -0,0 +1,12 @@
+#removed sql
+
+CREATE TABLE bears (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER,
+  gender CHAR(1),
+  color TEXT,
+  temperament TEXT,
+  alive BOOLEAN
+);
+
---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 40, "M", "grey", "docile", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 23, "F", "blue", "rude", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 2, "M", "red", "calm", "false");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 56, "M", "green", "crazy", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 13, "F", "yellow", "chaotic", "false");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 12, "M", "purple", "happy", "false");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 4, "F", "pink", "mad", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("null", 34, "M", "black", "senile", "false");
